dc08fc7dfbc223ac9dd0d6f7d6e929725bb49523
Karen Moyolema <kmoyolema3943@uta.edu.ec>
2026-04-19 17:34:35 -0500

    cambio del puerto para render

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Backend iniciado en http://localhost:${PORT}`);
